601b52768d333e7c10dee5c94917fe1e547896a0
kunal <2kunal6@gmail.com>
2026-06-23 11:23:50 +0530

    using nearest logic

--- src/kaggle/orbit_wars/main.py
+++ src/kaggle/orbit_wars/main.py
@@ -39,37 +39,64 @@ def nearest_planet_sniper(obs):
     if not targets:
         return moves
 
+    def touches_sun(x, y, a):
+        cx, cy = 50, 50
+        r = 10
+        dx = math.cos(a)
+        dy = math.sin(a)
+        vx = cx - x
+        vy = cy - y
+
+        t = vx * dx + vy * dy
+
+        if t < 0:
+            return False
+
+        closest_x = x + t * dx
+        closest_y = y + t * dy
+
+        dist2 = (closest_x - cx) ** 2 + (closest_y - cy) ** 2
+
+        return dist2 <= r * r
+
     for mine in my_planets:
         targets.sort(key=lambda p: math.sqrt((p.x - mine.x)**2 + (p.y - mine.y)**2))
+        max_ships = 0
+        max_ships_t = None
         for t in targets:
+            dist = math.sqrt((mine.x - t.x) ** 2 + (mine.y - t.y) ** 2)
+            if (dist > 10):
+                break
             if(t.ships >= mine.ships):
                 continue
-            def touches_sun(x, y, a):
-                cx, cy = 50, 50
-                r = 10
-                dx = math.cos(a)
-                dy = math.sin(a)
-                vx = cx - x
-                vy = cy - y
-
-                t = vx * dx + vy * dy
-
-                if t < 0:
-                    return False
-
-                closest_x = x + t * dx
-                closest_y = y + t * dy
-
-                dist2 = (closest_x - cx)**2 + (closest_y - cy)**2
-
-                return dist2 <= r * r
             if(is_rotating(t)):
                 continue
+            if(t.ships > max_ships):
+                max_ships_t = t
+                max_ships = t.ships
+        if(max_ships_t==None):
+            min_dist = float('inf')
+            for t in targets:
+                dist = math.sqrt((mine.x - t.x) ** 2 + (mine.y - t.y) ** 2)
+                if dist < min_dist:
+                    min_dist = dist
+                    nearest = t
 
-            dist = math.sqrt((mine.x - t.x)**2 + (mine.y - t.y)**2)
-            angle = math.atan2(t.y - mine.y, t.x - mine.x)
+            if nearest is None:
+                continue
+
+            # How many ships do we need? Target's garrison + 1
+            ships_needed = max(nearest.ships + 1, 20)
+
+            # Only send if we have enough
+            if mine.ships >= ships_needed:
+                # Calculate angle from our planet to the target
+                angle = math.atan2(nearest.y - mine.y, nearest.x - mine.x)
+                moves.append([mine.id, angle, ships_needed])
+        else:
+            angle = math.atan2(max_ships_t.y - mine.y, max_ships_t.x - mine.x)
             if(touches_sun(mine.x, mine.y, angle)==True):
                 continue
-            moves.append([mine.id, angle, t.ships+1])    
+            moves.append([mine.id, angle, max_ships_t.ships+1])
 
     return moves
